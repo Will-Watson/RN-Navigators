e6fb42ef6d3724b07961677250e64cc8b2dd520f
William Watson <williampwatson1991@gmail.com>
2024-01-14 20:06:49 -0500

    feat: added icons to the drawer

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
@@ -9,10 +10,36 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name='Welcome' component={WelcomeScreen} />
-        <Drawer.Screen name='User' component={UserScreen} />
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: '#3c0a6b' },
+          headerTintColor: '#fff',
+          drawerActiveBackgroundColor: '#3c0a6b',
+          drawerActiveTintColor: '#fff',
+          // drawerStyle: { backgroundColor: '#ccc' },
+        }}
+      >
+        <Drawer.Screen
+          name='Welcome'
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: 'Welcome Screen',
+            drawerIcon: ({ color, size }) => {
+              return <Ionicons name='home' size={size} color={color} />;
+            },
+          }}
+        />
+        <Drawer.Screen
+          name='User'
+          component={UserScreen}
+          options={{
+            drawerLabel: 'Welcome Screen',
+            drawerIcon: ({ color, size }) => {
+              return <Ionicons name='person' size={size} color={color} />;
+            },
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
-};
+}
